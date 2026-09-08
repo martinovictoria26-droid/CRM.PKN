90e14707b1af376d7d8be6b1f6d5cb5ee770e17d
martinovictoria26-droid <martinovictoria26@gmail.com>
2026-09-08 15:40:32 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
-
+// Configuración central del CRM PKN
+// No modificar este archivo al hacer cambios de diseño o funcionalidades.
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwFldRqF1S6Oq8M0XF_Fte-LLe30ZcfEy23C44D1-VoLuhY-XR46y7F82ICtgWLO-16mA/exec";
